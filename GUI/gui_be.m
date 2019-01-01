b348aae373743d21ea7549e7132e15a64d8343f7
Daniel Hamilton <d.hamilton@ufl.edu>
2019-01-01 16:26:29 -0500

    MATLAB GUI init

---
+++ GUI/gui_be.m
@@ -0,0 +1,12 @@
+classdef gui_be
+    %GUI back end properties and methods. Connects to GUI front end through
+    %app designer 
+    
+    properties
+    end
+    
+    methods
+    end
+    
+end
+
